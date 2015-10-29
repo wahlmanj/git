eea297f6b7c73db9f82315cef6ab05cbdd22bcfd
wahlmanj <wahlman.j@gmail.com>
2015-10-29 00:18:23 -0500

    Update clt.bash

--- clt.bash
+++ clt.bash
@@ -3,6 +3,7 @@
 # Check if command line tools are installed
 
 if [ -d /Library/Developer/CommandLineTools ]; then
+echo 'CLT exists'
 	else
 	osx_vers=$(sw_vers -productVersion | awk -F "." '{print $2}')
 cmd_line_tools_temp_file="/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress"
