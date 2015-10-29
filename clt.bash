a16aa9433b52c484716fee23524f15783de04850
wahlmanj <wahlman.j@gmail.com>
2015-10-28 23:13:17 -0500

    test

---
+++ clt.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Installing the Xcode command line tools on 10.7.x or higher
+
+osx_vers=$(sw_vers -productVersion | awk -F "." '{print $2}')
+cmd_line_tools_temp_file="/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress"
+
+# Installing the latest Xcode command line tools on 10.9.x or higher
+
+if [[ "$osx_vers" -ge 9 ]]; then
+
+	# Create the placeholder file which is checked by the softwareupdate tool 
+	# before allowing the installation of the Xcode command line tools.
+	
+	touch "$cmd_line_tools_temp_file"
+	
+	# Find the last listed update in the Software Update feed with "Command Line Tools" in the name
+	
+	cmd_line_tools=$(softwareupdate -l | awk '/\*\ Command Line Tools/ { $1=$1;print }' | tail -1 | sed 's/^[[ \t]]*//;s/[[ \t]]*$//;s/*//' | cut -c 2-)
+	
+	#Install the command line tools
+	
+	softwareupdate -i "$cmd_line_tools" -v
+	
+	# Remove the temp file
+	
+	if [[ -f "$cmd_line_tools_temp_file" ]]; then
+	  rm "$cmd_line_tools_temp_file"
+	fi
+fi
+
+exit 0
